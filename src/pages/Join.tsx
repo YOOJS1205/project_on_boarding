9a17194f9f46f7fe542948220d1c3083b2766c86
yoojs1205 <junsang8921@gmail.com>
2022-10-27 23:55:49 +0900

    init-pr branch 내용 전부 삭제

--- src/pages/Join.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import JoinForm from '../components/Form/JoinForm';
-
-export default function Join() {
-  return <JoinForm />;
-}
--- src/pages/Login.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import LoginForm from '../components/Form/LoginForm';
-
-export default function Login() {
-  return <LoginForm />;
-}
